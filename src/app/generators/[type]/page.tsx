8dbdf56c4ca2e19449771b86d3ebabb418a28d74
Tyrel Chambers <tychambers3@gmail.com>
2024-11-06 07:50:04 -0500

    more

--- src/app/generators/[type]/page.tsx
+++ src/app/generators/[type]/page.tsx
@@ -1,7 +1,8 @@
 import { Generator, getGenerator } from "@/generator-types";
 import React from "react";
+import { PageProps } from "../../../../.next/types/app/page";
 
-const Page = async ({ params }: { params: { type: string } }) => {
+const Page = async ({ params }: PageProps) => {
   const type = (await params).type as Generator;
   const Gen = getGenerator(type);
 
