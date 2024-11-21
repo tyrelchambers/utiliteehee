a2db1d84f4e002748e9847e0c92e430cfa8d69e0
Tyrel Chambers <tychambers3@gmail.com>
2024-11-21 14:52:59 -0500

    remove CL

--- src/generator-types/RandomRomanFact.tsx
+++ src/generator-types/RandomRomanFact.tsx
@@ -4,7 +4,6 @@ import React from "react";
 
 const RandomRomanFact = async () => {
   const fact = await getRomanEmpireFacts();
-  console.log(fact);
 
   return (
     <section className="section">
